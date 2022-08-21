a1ca8d45fd6e5520ee66e5874f43bf8c1f3cd6aa
jduan <jduan@users.noreply.github.com>
2022-08-21 19:07:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jduan"
+original_name="hackattic"
+original_urlname="hackattic"
+original_description="Awesome hackattic created by jduan"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv hackattic $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
